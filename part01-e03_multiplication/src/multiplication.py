7f6bc9e6487a28769f8c54a7c95a1cdadcb30455
Zumh <17211423+Zumh@users.noreply.github.com>
2023-10-31 14:43:07 -0400

    working on if statment and loop

--- part01-e03_multiplication/src/multiplication.py
+++ part01-e03_multiplication/src/multiplication.py
@@ -2,6 +2,9 @@
 
 def main():
     # Enter your solution here
-
+    number = 4
+    for first in range(11):
+        product = first * number
+        print(f"{number} multiplied by {first} is {product}")
 if __name__ == "__main__":
     main()
--- part01-e04_multiplication_table/src/multiplication_table.py
+++ part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,13 @@
 
 
 def main():
-    pass
+    for rowNumber in range(1, 11):
+        for columnNumber in range(1, 11):
+            product = rowNumber * columnNumber
+            # formatting the product variable with a width of four characters
+            print(f"{product:4}", end="")
+
+        print()
 
 if __name__ == "__main__":
     main()
