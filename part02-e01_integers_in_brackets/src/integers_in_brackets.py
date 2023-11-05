9f2dce9eceba297a9c9c3859d8f21e4597b9a371
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-05 17:22:29 -0500

    working on reges

--- part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 
+import re
 def integers_in_brackets(s):
-    return []
+    pattern = r'\[[-\d]+\]'
+    return list(map(int, re.findall(pattern, s)))    
 
 def main():
-    pass
+    result = integers_in_brackets(" afd [asd] [12 ] [a34] [ -43 ]tt [+12]xxx")
+    print(result)
+
 
 if __name__ == "__main__":
     main()
