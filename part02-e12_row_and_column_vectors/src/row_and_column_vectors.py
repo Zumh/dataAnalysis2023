c9ae4f79d4fbeb5704bfbfa9fecd56bcba893d3f
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-07 15:11:02 -0500

    p2 e12 work with np.split

--- part02-e12_row_and_column_vectors/src/row_and_column_vectors.py
+++ part02-e12_row_and_column_vectors/src/row_and_column_vectors.py
@@ -3,10 +3,17 @@
 import numpy as np
 
 def get_row_vectors(a):
-    return list(a)
+    n, m = a.shape
+    # Use np.split to split the matrix a long axis 0 ( rows )
+    row_vectors = np.split(a, n)
+
+    return row_vectors
 
 def get_column_vectors(a):
-    return []
+    n, m = a.shape
+    # Use np.split to split the matrix a long axis 0 ( rows )
+    column_vectors = np.split(a, m, axis = 1)
+    return column_vectors
 
 def main():
     np.random.seed(0)
