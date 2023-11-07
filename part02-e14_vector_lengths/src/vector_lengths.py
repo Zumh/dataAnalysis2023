cb26762ab37a23483b1697e6671bd1e2fe01fa9b
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-07 18:58:57 -0500

    vector length is done

--- part02-e14_vector_lengths/src/vector_lengths.py
+++ part02-e14_vector_lengths/src/vector_lengths.py
@@ -3,11 +3,32 @@
 import numpy as np
 #import scipy.linalg
 
+
 def vector_lengths(a):
-    return np.array([])
+    # return an array of shape (n,)
+    # that has the length of each vector in the input. 
+    # Euclid norm for ||V|| = sqrt(3^2 + 4^2)
+    # use vectorized operations. 
+    # the np.sum and the np.sqrt functions. 
+    
+    # square
+    value  = a**2
+    value = value.sum(axis=1)
+    value = np.sqrt(value)
+
+
+  
+
+    return np.array(value)
+
 
 def main():
-    pass
+    n=10
+    for m in range(1,5):
+        a=np.random.randn(n, m)
+        result = vector_lengths(a)
+        if result.shape == (n,):
+            print(f"{result.shape}")
 
 if __name__ == "__main__":
     main()
