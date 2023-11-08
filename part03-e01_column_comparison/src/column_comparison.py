c8ed1b5490df54b8c4ae8786096ab4bd42cd2cb6
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-07 21:38:26 -0500

    column comparison

--- part03-e01_column_comparison/src/column_comparison.py
+++ part03-e01_column_comparison/src/column_comparison.py
@@ -3,10 +3,27 @@
 import numpy as np
 
 def column_comparison(a):
-    return np.array([])
+    """
+    The function should return a new array containing those rows 
+    from the input that have the value in the second column larger than
+    in the second last column.
+    """
+    second_column = a[:,1]
+    second_last_column = a[:,-2]
+
+    larger_than_bool = (second_column > second_last_column)
+    larger_than_rows = a[larger_than_bool]
+   
+    return larger_than_rows
     
 def main():
-    pass
+    matrix = np.array([[8, 9, 3, 8, 8],
+                  [0, 5, 3, 9, 9],
+                  [5, 7, 6, 0, 4],
+                  [7, 8, 1, 6, 2],
+                  [2, 1, 3, 5, 8]])
+    result=column_comparison(matrix)
+    print(result)
 
 if __name__ == "__main__":
     main()
