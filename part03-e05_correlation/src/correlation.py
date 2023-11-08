039509059f74441203c94647950ac5ddf7da2dc3
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-08 14:13:02 -0500

    correlation is done

--- part03-e05_correlation/src/correlation.py
+++ part03-e05_correlation/src/correlation.py
@@ -6,12 +6,22 @@ import numpy as np
 def load():
     import pandas as pd
     return pd.read_csv("src/iris.csv").drop('species', axis=1).values
+    #return pd.read_csv("iris.csv").drop('species', axis=1).values
 
 def lengths():
-    return 0
+    # What is the Pearson correlation between the variables sepal length and petal length
+    data = load()
+    sepal_length, petal_length = data[:,0], data[:,2]
+    length_correlation = scipy.stats.pearsonr(sepal_length, petal_length)[0]
+
+    return length_correlation
 
 def correlations():
-    return np.array([])
+    data = load()
+    
+    result = np.corrcoef(data,  rowvar=False)
+    
+    return result
 
 def main():
     print(lengths())
