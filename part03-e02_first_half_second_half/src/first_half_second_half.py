52af0537bedadc372cf5e59a2fbcb13571230cdb
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-07 21:54:21 -0500

    first_half second half

--- part03-e02_first_half_second_half/src/first_half_second_half.py
+++ part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -3,10 +3,19 @@
 import numpy as np
 
 def first_half_second_half(a):
-    return np.array([])
+    n, m = a.shape
+    mid = m//2
+    first_half = np.sum(a[:, :mid], axis = 1)
+    last_half = np.sum(a[:, mid:], axis = 1)
+
+    first_half_larger_row = a[(first_half > last_half)]
+    return (first_half_larger_row)
+
 
 def main():
-    pass
+    a = np.array([[1, 3, 4, 2],
+              [2, 2, 1, 2]])
+    first_half_second_half(a)
 
 if __name__ == "__main__":
     main()
