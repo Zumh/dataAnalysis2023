2c09c652a138ceea2fb601c0c7985bc2162b60d1
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-08 21:13:14 -0500

    matplot libs p3 e9 done

--- part03-e09_multiple_graphs/src/multiple_graphs.py
+++ part03-e09_multiple_graphs/src/multiple_graphs.py
@@ -3,7 +3,16 @@
 import matplotlib.pyplot as plt
 
 def main():
-    pass
+    x1 = [2,4,6,7]
+    y1 = [4,3,5,1]
+    x2 = [1,2,3,4]
+    y2 = [4,2,3,1]
+    plt.plot(x1, y1)
+    plt.plot(x2, y2)
+    plt.title("title")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.show()
 
 if __name__ == "__main__":
     main()
