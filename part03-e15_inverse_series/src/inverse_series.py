0bad930b4aa11e66871f6f7bebefb51fed5cd814
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-10 19:57:37 -0500

    pandas inverse_series done

--- part03-e15_inverse_series/src/inverse_series.py
+++ part03-e15_inverse_series/src/inverse_series.py
@@ -3,10 +3,22 @@
 import pandas as pd
 
 def inverse_series(s):
-    return pd.Series()
+    """
+        Write function inverse_series that get a Series as a parameter and 
+        returns a new series, whose indices and values have swapped roles. 
+        Test your function from the main function.
+    """
+
+    
+    return pd.Series(s.index, s.values)
 
 def main():
-    return
+    L=[1,2,3,1]
+    ind=list("abcd")
+    s = pd.Series(L, index=ind)
+    print(s)
+    t = inverse_series(s)
+    print(t)
 
 if __name__ == "__main__":
     main()
