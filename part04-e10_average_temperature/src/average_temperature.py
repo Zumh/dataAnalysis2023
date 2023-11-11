26fe2e1e371a7a16e282d64c72a171fecb2ef42e
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-11 18:32:27 -0500

    average_temperature is done

--- part04-e10_average_temperature/src/average_temperature.py
+++ part04-e10_average_temperature/src/average_temperature.py
@@ -3,10 +3,16 @@
 import pandas as pd
 
 def average_temperature():
-    return 0.0
+    """returns the average temperature in July."""
+    df = pd.read_csv("src/kumpula-weather-2017.csv", sep=',')
+    month = 7
+    target_month = df[df.loc[:, 'm'] == month]
+    temperature = target_month['Air temperature (degC)']
+    return sum(temperature)/len(temperature)
 
 def main():
-    return
+    avg = average_temperature()
+    print(f"Average temperature in July: {avg:.1f}")
 
 if __name__ == "__main__":
     main()
