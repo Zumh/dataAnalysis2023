a73e37d9759b8ade17429aa1bfa1550db4513493
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-11 13:01:14 -0500

    municipal_information is done

--- part04-e03_municipal_information/src/municipal_information.py
+++ part04-e03_municipal_information/src/municipal_information.py
@@ -3,7 +3,15 @@
 import pandas as pd
 
 def main():
-    return
+    # seperator is tabulator \t
+    data_frame = pd.read_csv("src/municipal.tsv", sep='\t')
+    #print(data_frame.head()) # print the first five rows
+    r, c = data_frame.shape
+
+    print(f"Shape: {r},{c}")
+    print("Columns:")
+    for col in data_frame.columns.tolist():
+        print(col)
 
 
 if __name__ == "__main__":
