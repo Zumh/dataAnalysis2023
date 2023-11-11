e37ee33afa04161742fdebdb1bc1d27aaccc326a
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-11 11:52:56 -0500

    power series is done

--- part04-e02_powers_of_series/src/powers_of_series.py
+++ part04-e02_powers_of_series/src/powers_of_series.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 
-import pandas as pd
 
+
+import pandas as pd
+import numpy as np
 def powers_of_series(s, k):
-    return None
+
+    
+    # create pd dataframes here 
+    data = pd.DataFrame(s, columns=[1])
+    
+
+    for number in range(1, k+1):
+      
+        data[number] = data[1] ** number
+
+    return data
     
 def main():
-    return
+    s = pd.Series([1,2,3,4], index=list("abcd"))
+    k = 3
+    df = powers_of_series(s, k)
+    print(df)
     
 if __name__ == "__main__":
     main()
