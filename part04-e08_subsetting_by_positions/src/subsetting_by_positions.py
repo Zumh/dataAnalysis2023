2723ce8126bd065586d595d0b99cf7198073bd4e
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-11 17:59:17 -0500

    subsetting_by_positions is done

--- part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
+++ part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -2,11 +2,17 @@
 
 import pandas as pd
 
+
 def subsetting_by_positions():
-    return pd.DataFrame()
+    #return pd.DataFrame()
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep='\t')
+    # top 10 with Title and Artist
+    return (df.iloc[0:10, [2,3]])
+ 
 
 def main():
-    return
+    result = subsetting_by_positions()
+    print(result)
 
 if __name__ == "__main__":
     main()
