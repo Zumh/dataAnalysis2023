bc75e69709be99bae9884c867dcb2c762498efd0
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-11 18:19:26 -0500

    snow_depth is done

--- part04-e09_snow_depth/src/snow_depth.py
+++ part04-e09_snow_depth/src/snow_depth.py
@@ -3,10 +3,20 @@
 import pandas as pd
 
 def snow_depth():
-    return 0.0
+    """returns the maximum amount of snow in the year 2017."""
+    # read csv file and calculate the satistics
+    df = pd.read_csv("src/kumpula-weather-2017.csv", sep=',')
+    snow_amount = 'Snow depth (cm)'
+  
+    # extract rows from year 2017
+    target_year = df[df.loc[:, "Year"] == 2017]
+    max_snow_amount = max(target_year.loc[:,snow_amount])
+    
+    return max_snow_amount
 
 def main():
-    return
+    result = snow_depth()
+    print(f"Max snow depth: {result:.1f}")
 
 if __name__ == "__main__":
     main()
