7c71d2460e4e159e94ca963601cdd5a12f4671d0
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-10 20:20:55 -0500

    pandas cities part04e1 done

--- part04-e01_cities/src/cities.py
+++ part04-e01_cities/src/cities.py
@@ -3,10 +3,26 @@
 import pandas as pd
 
 def cities():
-    return None
+    indicies = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
+    datas = [
+        [643272, 715.48],
+        [279044, 528.03],
+        [231853, 689.59],
+        [223027, 240.35],
+        [201810, 3817.52]
+        ]
+    return pd.DataFrame(datas, columns=["Population", "Total area"],index=indicies)
+   
     
 def main():
+    df = cities()
+    cols=df.columns
+    ind=df.index
+    print(cols)
+    print(ind)
+    print(df)
     return
+main()
     
 if __name__ == "__main__":
     main()
