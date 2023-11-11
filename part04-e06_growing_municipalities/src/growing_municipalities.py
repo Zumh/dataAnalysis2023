0b960ccd8370fbaa7f60ced0bc3a8dd714ac9b6b
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-11 16:42:42 -0500

    grpwomg ,imoco[a;otoes is done

--- part04-e06_growing_municipalities/src/growing_municipalities.py
+++ part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -3,10 +3,21 @@
 import pandas as pd
 
 def growing_municipalities(df):
-    0.0
+    # returns the proportion of municipalities with increasing population in that subset.
+    # check change of population from above 0
+    increasing_pop = df[df["Population change from the previous year, %"]>0]
+    value = (len(increasing_pop) / len(df))
+    return value
 
 def main():
-    return
+    path = "src/municipal.tsv"
+    df = pd.read_csv(path, sep='\t', index_col='Region 2018')
+    # extract manuciplities of findland 'Akaa':'Äänekoski'
+    findland_manucipal = df['Akaa':'Äänekoski']
+
+    
+    growing_proportion = growing_municipalities(findland_manucipal)
+    print(f"Proportion of growing municipalities: {growing_proportion:.1f}%")
 
 if __name__ == "__main__":
     main()
