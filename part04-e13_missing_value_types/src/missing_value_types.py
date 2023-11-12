1a077821305fd0ebdfa0cf35645431629c1114d1
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-12 10:37:15 -0500

    missing value is done

--- part04-e13_missing_value_types/src/missing_value_types.py
+++ part04-e13_missing_value_types/src/missing_value_types.py
@@ -4,10 +4,24 @@ import pandas as pd
 import numpy as np
 
 def missing_value_types():
-    return None
+    indecies = ["United Kingdom", "Finland", "USA", "Sweden", "Germany", "Russia"]
+
+    presidents = [np.nan, 'Niinistö', 'Trump', np.nan, 'Steinmeier', 'Putin']
+    years = [np.nan, 1917, 1776, 1523, np.nan, 1992]
+    datas = {
+        'Year of independence':years,
+        'President':presidents
+    }
+  
+    result = pd.DataFrame(datas, index=indecies)
+  
+  
+
+    return(result)
                
 def main():
-    return
+    result = missing_value_types()
+    print(result)
 
 if __name__ == "__main__":
     main()
