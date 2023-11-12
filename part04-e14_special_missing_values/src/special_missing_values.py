95cda7aa4c7e64aef8730e7d7a7ba9e9a57ad45f
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-12 12:28:56 -0500

    special missing value is done

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -4,9 +4,25 @@ import pandas as pd
 import numpy as np
 
 def special_missing_values():
-    return None
+
+    # read the file and transform in to data frame 
+
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep='\t')
+    df['LW'] = df['LW'].replace(['New', 'Re'], None)
+    df['LW'] = pd.to_numeric(df['LW'], errors='coerce')
+    return(df[df['Pos'] > df['LW']])
+""" 
+def special_missing_values():
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    m = (df["LW"] == "New") | (df["LW"] == "Re")
+    df.loc[m, "LW"] = np.nan
+    df["LW"] = pd.to_numeric(df["LW"])
+    m2 = df["LW"] < df["Pos"]
+    return df[m2]"""
 
 def main():
+    df = special_missing_values()
+    print(df.shape ==  (17,7))
     return
 
 if __name__ == "__main__":
