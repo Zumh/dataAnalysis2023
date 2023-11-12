9e3e0b7dfc56694d14502da5b4bf6854bb989890
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-12 12:31:01 -0500

    special missing value is done

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -11,14 +11,7 @@ def special_missing_values():
     df['LW'] = df['LW'].replace(['New', 'Re'], None)
     df['LW'] = pd.to_numeric(df['LW'], errors='coerce')
     return(df[df['Pos'] > df['LW']])
-""" 
-def special_missing_values():
-    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
-    m = (df["LW"] == "New") | (df["LW"] == "Re")
-    df.loc[m, "LW"] = np.nan
-    df["LW"] = pd.to_numeric(df["LW"])
-    m2 = df["LW"] < df["Pos"]
-    return df[m2]"""
+
 
 def main():
     df = special_missing_values()
