24e63dd1aae07bcdad1dd8e3311d818bc23f4134
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-14 13:44:46 -0500

    suicide fraction is done

--- part05-e06_suicide_fractions/src/suicide_fractions.py
+++ part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -2,11 +2,21 @@
 
 import pandas as pd
 
+
 def suicide_fractions():
-    return pd.Series()
+    # top40UK
+    who_suicide_stats = pd.read_csv("src/who_suicide_statistics.csv", sep=',')
+
+    # the mean fraction of suicides per population in that country
+    who_suicide_stats['suicide_fraction'] = who_suicide_stats['suicides_no']/ who_suicide_stats['population']
+
+    average_suicide = who_suicide_stats.groupby('country')['suicide_fraction'].mean()
+    
+    return (average_suicide)
+ 
 
 def main():
-    return
+    suicide_fractions()
 
 if __name__ == "__main__":
     main()
