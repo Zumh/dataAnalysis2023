b32c0498a83cf57b733de5a808cae05b915636cd
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-13 23:41:06 -0500

    top bands is done

--- part05-e03_top_bands/src/top_bands.py
+++ part05-e03_top_bands/src/top_bands.py
@@ -3,10 +3,18 @@
 import pandas as pd
 
 def top_bands():
-    return pd.DataFrame()
+    # top40UK
+    top40UK = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep='\t')
+    bands = pd.read_csv("src/bands.tsv", sep='\t')
+    bands['Band'] = bands['Band'].str.title()
+    top40UK['Artist'] =  top40UK['Artist'].str.title()
+  
+    merge_data = pd.merge(top40UK, bands, left_on='Artist', right_on='Band')
+   
+    return merge_data
 
 def main():
-    return
+    top_bands()
 
 if __name__ == "__main__":
     main()
