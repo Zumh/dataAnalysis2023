12d7a7d5cb5156a049893b25d4a003584b9be866
Zumh <17211423+Zumh@users.noreply.github.com>
2023-11-14 12:50:54 -0500

    best record company is done

--- part05-e05_best_record_company/src/best_record_company.py
+++ part05-e05_best_record_company/src/best_record_company.py
@@ -3,11 +3,26 @@
 import pandas as pd
 
 def best_record_company():
-    return None
+    # top40UK
+    top40UK = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep='\t')
+    print(top40UK.shape)
+
+    # here we going to group by WoC and sum
+    publisher_stats = top40UK.groupby('Publisher')['WoC'].sum()
+    
+ 
+    # find the best publisher by maximum WoC sum. 
+    # it return the index or key of maximum value from pandas series
+    best_pubisher = publisher_stats.idxmax()
+
+    # now we extract data frame of the best publisher from the original top40UK
+    best_pubisher_record = top40UK[top40UK['Publisher'] == best_pubisher]
+ 
+    return best_pubisher_record
 
 def main():
-    return
-    
+    best_record_company()
+
 
 if __name__ == "__main__":
     main()
